b6eac901dba6046b671cfcff3ab8319c13390590
Yubraj Dhakal (Upamanyu-coder) <Upamanyu-coder@users.noreply.github.com>
2025-08-10 22:53:45 +0545

    Finals Variables

---
+++ 01_basics/variables02.js
@@ -0,0 +1,36 @@
+let firstName = "Kreetika";
+let lastName = "Sharma";
+let age = 25;
+let isEmployed = true;
+let hobbies = ["reading", "coding", "traveling"];
+let address = {
+  city: "Bangalore",
+  state: "Karnataka",
+  country: "India",
+};
+
+age = 21; // Reassigning age
+console.log("First Name:", firstName);
+console.log("Last Name:", lastName);
+lastName = "Dhakal"; // Reassigning lastName
+console.log("Age:", age);
+console.log("Is Employed:", isEmployed);
+console.log("Hobbies:", hobbies);
+console.log("Address:", address);
+// console.table([
+//   "firstName:",
+//   firstName,
+//   "lastName:",
+//   lastName,
+//   "age:",
+//   age,
+//   "isEmployed:",
+//   isEmployed,
+//   "hobbies:",
+//   hobbies,
+
+//   address,
+// ]);
+
+const lastName2 = "Dhakal";
+console.log("Full Name : ", firstName, lastName2);
---
+++ 01_basics/variables03.js
@@ -0,0 +1,22 @@
+const countryCode = "+977";
+const countryName = "Nepal";
+
+let employeeName = "Kreetika Sharma";
+let contactNo = "9841234567";
+
+employeeName = "Kreetika Dhakal"; // Reassigning employeeName
+
+let population1 = 30_000_000; // Using underscore for better readability
+let population2 = 29_000_000;
+let population3 = 30_100_000;
+
+console.table([
+  employeeName,
+  countryName,
+  countryCode,
+  contactNo,
+  "Population Of Nepal 2082 = ",
+  population1,
+  population2,
+  population3,
+]);
