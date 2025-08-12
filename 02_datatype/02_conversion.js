fea67a880432fe91de1fa2c0d65226c1f906f78c
Yubraj Dhakal (Upamanyu-coder) <Upamanyu-coder@users.noreply.github.com>
2025-08-12 22:10:27 +0545

    3rd

--- 02_datatype/02_conversion.js
+++ 02_datatype/02_conversion.js
@@ -3,9 +3,22 @@ let score = "33";
 console.log(typeof score);
 
 // score = String(score);
-score = Number(score);
-console.log(typeof score);
+// score = Number(score);
+// console.log(typeof score);
 
-let valueInNumber = Number("33abc");
-console.log(valueInNumber);
-console.log(typeof valueInNumber);
+// let valueInNumber = Number("33abc");
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+let abs = 21;
+
+let abc = String(abs);
+
+console.log(typeof abs);
+console.log(typeof abc);
+console.log(abc);
+
+console.log(Number("abc"));
+
+abc = Number(abc);
+console.log(typeof abc);
