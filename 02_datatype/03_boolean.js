531a016521d67396b1134eb376dac99c913713eb
Yubraj Dhakal (Upamanyu-coder) <Upamanyu-coder@users.noreply.github.com>
2025-08-27 21:50:24 +0545

    Strings

---
+++ 02_datatype/03_boolean.js
@@ -0,0 +1,17 @@
+let a = 15;
+let b = 34;
+let c = 67;
+
+console.log(c <= b);
+console.log(a != b);
+
+// =================  null and undefine ===================  \\
+
+console.log(null > a);
+console.log(undefined == 0);
+console.log(undefined <= b);
+
+console.log(null);
+console.log(undefined);
+
+
---
+++ 02_datatype/04_Strings.js
@@ -0,0 +1,15 @@
+const name = "Yubraj Dhakal Upamanyu";
+const greeting = "Hello, World!";
+const age = 25;
+const height = 5.9;
+
+// console.log(
+//   'Good Morning and ${greeting}, My name is ${name}. I am ${age} years old and my height is ${height} feet.
+// );
+
+const fullName = name;
+
+console.log(fullName.length); // Length of the string
+console.log(fullName.toUpperCase()); // Convert to uppercase
+console.log(fullName.charAt(3));
+console.log(fullName.indexOf("a")); // Find index of substring
