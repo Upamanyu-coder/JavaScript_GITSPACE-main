5ab2e7890dc8ecc674805c8f21c35a948be4a2f2
Yubraj Dhakal (Upamanyu-coder) <Upamanyu-coder@users.noreply.github.com>
2025-12-19 21:00:37 +0545

    Loops

---
+++ 04_Loops/01_sum1-5.js
@@ -0,0 +1,13 @@
+/* Write a 'while' loop that calculates the sums of
+ all numbers from 1 to 5 and stores the results
+ in variable named 'sum'. */
+
+let sum = 0;
+let i = 0;
+
+while (i <= 5) {
+  sum += i;
+  i++;
+}
+
+console.log(sum);
