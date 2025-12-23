df75e90ceddc1da4ded771796ce9f392ab513eb8
Yubraj Dhakal (Upamanyu-coder) <Upamanyu-coder@users.noreply.github.com>
2025-12-23 19:44:52 +0545

    06 loops

--- 04_Loops/06_multiplies.js
+++ 04_Loops/06_multiplies.js
@@ -1,9 +1,9 @@
-Question: Write a 'for' loop that multiplies each elements in the array '[2,4,6]' by 2 and logs the result in a new array named 'multipliedNumbers'.
+// Question: Write a 'for' loop that multiplies each elements in the array '[2,4,6]' by 2 and logs the result in a new array named 'multipliedNumbers'.
 let numbers = [2, 4, 6];
 let multipliedNumbers = [];
 
 for (let i = 0; i < numbers.length; i++) {
-      multipliedNumbers.push(numbers[i] * 2);
+  multipliedNumbers.push(numbers[i] * 2);
 }
 
 console.log(multipliedNumbers);
